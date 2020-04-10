b0e6ad7386137f645e2db33efb755a0299e1af7b
Enrique López Flores <elopez@jornada.com.mx>
2020-04-10 01:52:55 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Mexflix
+
+Se trata de un sistema web construido en PHP y MySQL.
+
+El autor original es Jon Mircha, (Jonahan Miranda Charles ).
+
+Mexflix se construyó a partir del patrón Modelo Vista Controlador (MVC), y mediante Programación Orientada a Objetos (POO).
+
+Mi agradecimiento total @Mircha por las valiosas horas de capacitación que me ha brindado.
+
+Marzo de 2015.
---
+++ controllers/Autoload.php
@@ -0,0 +1,22 @@
+<?php
+
+class AutoLoad {
+
+  public function __construct(){
+      spl_autoload_register(function($class_name){
+      $models_path      = './models/' . $class_name . '.php';
+      $controllers_path = './controllers/' . $class_name . '.php';
+
+      if(file_exists($models_path)) echo "<p>$models_path</p>";
+      if(file_exists($controllers_path)) echo "<p>$controllers_path</p>";
+
+      if(file_exists($models_path)) require_once($models_path);
+      
+      if(file_exists($controllers_path))  require_once($controllers_path);
+
+    });
+
+
+  }
+
+}
